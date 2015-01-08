6efe1b72c32c2fdbc14659c12fc6202e563ab1ee
Yann SECQ <yann.secq@gmail.com>
2015-01-08 21:55:36 +0100

    ajout de testListAllUsers (fonctionne avec JSON, mais pas pour la désérialisation automatique de Collection ...)

--- src/test/java/fr/iutinfo/TestUser.java
+++ src/test/java/fr/iutinfo/TestUser.java
@@ -1,8 +1,13 @@
 package fr.iutinfo;
 
+import java.util.List;
+
 import javax.ws.rs.core.Application;
+
 import org.glassfish.jersey.test.JerseyTest;
+
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 
@@ -29,5 +34,15 @@ public class TestUser extends JerseyTest {
 		User user1 = target("/user/foo").request().get(User.class);
 		User user2 = target("/user/foo").request().get(User.class);
 		assertEquals(user1, user2);
+	}
+	
+	@Test
+	public void tesListAllUsers() {
+		User user1 = target("/user/foo1").request().get(User.class);
+		User user2 = target("/user/foo2").request().get(User.class);
+		//List<User> users = target("/user/").request().get(List.class);
+		String userS = target("/user/").request().get(String.class);
+		System.out.println(userS);
+		//assertTrue(users.size() == 2);
 	}
 }
