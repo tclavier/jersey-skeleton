b82c2da4e02513affe12a8268425bfc205bc096b
Yann SECQ <yann.secq@gmail.com>
2015-01-08 21:52:03 +0100

    ajout de getUsers (avec paramètre optionel et valeur par défaut)

--- src/main/java/fr/iutinfo/User.java
+++ src/main/java/fr/iutinfo/User.java
@@ -1,12 +1,17 @@
 package fr.iutinfo;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/user")
@@ -39,5 +44,13 @@ public class User {
 			return user;
 		}
 		return users.get(name);
+	}
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<User> getUsers(
+			@DefaultValue("10") @QueryParam("limit") int limit) {
+		return new ArrayList<User>(users.values());
 	}
 }
