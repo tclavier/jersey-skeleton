cfd016f51fcf84d626e0f28954ab8996909fd322
Yann SECQ <yann.secq@gmail.com>
2015-01-10 23:40:00 +0100

    Ajout d'une méthode PUT de mise à jour du nom d'un utilisateur.

--- src/main/java/fr/iutinfo/UserResource.java
+++ src/main/java/fr/iutinfo/UserResource.java
@@ -3,15 +3,18 @@ package fr.iutinfo;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/user")
 @Produces(MediaType.APPLICATION_JSON)
@@ -26,15 +29,45 @@ public class UserResource {
 		return user;
 	}
 	
-	@GET
-	@Path("/{name}")
-	public User getUser(@PathParam("name") String name ) {
+	protected User find(String name) {
 		User out = null;
 		for (User user : users) {
 			if (user.getName().equals(name)) {
-				out = user;
+				return user;
 			}
 		}
+		return out;
+	}
+	protected User find(int id) {
+		User out = null;
+		for (User user : users) {
+			if (id == user.getId()) {
+				return user;
+			}
+		}
+		return out;
+	}
+	
+	@PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+	public Response updateUser(@PathParam("id") int id, 
+			User user) {
+		User oldUser = find(id);
+		System.out.println("Should update user with id: "+id
+				+" ("+oldUser+") to " +user);
+		if (user == null) {
+			throw new WebApplicationException(404);
+		}
+		oldUser.setName(user.getName());
+		return Response.status(200).entity(oldUser).build();
+	}
+	
+	@GET
+	@Path("/{name}")
+	public User getUser(@PathParam("name") String name ) {
+		User out = find(name);
 		if (out == null) {
 			throw new WebApplicationException(404);
 		} 
