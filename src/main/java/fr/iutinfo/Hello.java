32923e0c430c03b9a72d06b881cda0e95486bc0e
Thomas Clavier <tom@tcweb.org>
2015-01-13 21:05:56 +0100

    add sample servlet

---
+++ src/main/java/fr/iutinfo/Hello.java
@@ -0,0 +1,29 @@
+package fr.iutinfo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "testServlet", urlPatterns = { "/hello" }, initParams = { @WebInitParam(name = "simpleParam", value = "paramValue") })
+public class Hello extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		String simpleParam = getServletConfig().getInitParameter("simpleParam");
+		out.println("Hello World " + simpleParam);
+		out.close();
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
