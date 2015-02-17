cccb53ce79b7d2d0c48e02a118ec0390954dc8b3
Damien Masson <dma.truc@gmail.com>
2015-02-18 00:47:58 +0100

    Ajout de la Map

--- src/main/webapp/scripts/main.js
+++ src/main/webapp/scripts/main.js
@@ -1,17 +1,25 @@
 requirejs.config({
-    baseUrl: "libs",
+    baseUrl: "",
     paths: {
-        "jquery": "jquery",
-        "boostrap": "bootstrap"
+        "jquery": "libs/jquery",
+        "libs/boostrap": "libs/bootstrap",
+        "map": "scripts/map"
     },
 
     shim: {
-        "bootstrap": {
+        "libs/bootstrap": {
             deps: ["jquery"]
         }
     }
 });
 
-require(["jquery", "bootstrap"], function ($) {
+require(["jquery", "libs/bootstrap", "map"], function ($) {
+    var Map = require("map");
+
+    var map = new Map(null, [[0, 0],[1, 0]], 100, 100);
+
+    map.render($("#map")[0].getContext('2d'));
+
+
     console.log("main loaded");
 });
---
+++ src/main/webapp/scripts/map.js
@@ -0,0 +1,32 @@
+define(["jquery"],  function(require) {
+    return function Map(game, tiles, width, height) {
+        this.game = game;
+        this.tiles = tiles;
+        this.width = width;
+        this.height = height;
+        this.tile_size = (width/tiles.length);
+
+        var tilesColors = ["#EEEEEE", "#222222", "#EEEEEE", "#FFFF00", "#55FF55"];
+
+        this.render = function render(context) {
+            for (var xx = 0; xx < tiles.length; ++xx) {
+                for (var yy = 0; yy < tiles[0].length; ++yy) {
+                    var id = tiles[yy][xx];
+                    context.fillStyle = "#EEEEEE";
+                    if (id < tilesColors.length) {
+                        context.fillStyle = tilesColors[id];
+                    }
+
+                    context.fillRect(xx*this.tile_size, yy*this.tile_size, this.tile_size, this.tile_size);
+                    context.strokeRect(xx*this.tile_size, yy*this.tile_size, this.tile_size, this.tile_size);
+                }
+            }
+        }
+
+        this.update = function update(delta) {
+
+        }
+    }
+
+
+});
