67d2523f050299eed0db75be9744600b978dfee8
Louis Guilbert <louis.guilbert@free.fr>
2015-02-21 10:09:08 +0100

    Add some HTML and JS to handle pages

---
+++ src/main/webapp/scripts/page_controller.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	
+	// On affiche la page de login par défaut
+	var activeMenu = $("#login_bar");
+	var activeElement = $("#login_pane");
+	activeMenu.addClass("active_bar")
+	activeElement.show();
+	
+	
+	
+	// affichage de la page lors d'un clique
+	$(".menu_bar_element").click(function() {
+		// récupérer l'élément panel attaché à l'élément du menu
+		var newElement = $("#" + $(this).attr("for"));
+		activeElement.hide();
+		newElement.show();
+		
+		activeMenu.removeClass("active_bar");
+		$(this).addClass("active_bar");
+		
+		activeElement = newElement;
+		activeMenu = $(this);
+	});
+	
+	
+	// ouverture d'un niveau lors d'un clique sur le niveau dans la liste
+	$(".level_preview").click(function() {
+		// Chargement du niveau en AJAX
+		// TODO
+		
+		// Affichage du panel du niveau
+		newElement = $("#game_pane");
+		activeElement.hide();
+		newElement.show();
+		activeElement = newElement;
+	});
+		
+});
