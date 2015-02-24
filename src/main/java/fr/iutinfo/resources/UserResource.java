819bc530d02816a66b6438a37070ba7413956b2e
Louis Guilbert <louis.guilbert@etudiant.univ-lille1.fr>
2015-02-24 15:06:59 +0100

    vérif lors de l'inscription plus précise

--- src/main/java/fr/iutinfo/resources/UserResource.java
+++ src/main/java/fr/iutinfo/resources/UserResource.java
@@ -21,41 +21,75 @@ import fr.iutinfo.utils.Utils;
 public class UserResource {
 	private static UserDao dao = App.dbi.open(UserDao.class);
 
-	public UserResource() {
-		/*try {
-			dao.createUserTable();
-		} catch (Exception e) {
-			System.out.println("Table déjà là !");
-		}*/
-	}
-	
+	public UserResource() {}
+
+	/**
+	 * Insert l'utilisateur si celui ci est valide.
+	 * @param user
+	 * @return
+	 */
 	@POST
 	@Path("/register")
 	public Feedback createUser(User user) {
+		Feedback fb = null;
 		try {
-			User u = dao.isNameExist(user.getName());
-			if(u != null)
-				return new Feedback(false, "Le pseudo est déjà utilisé");
-			
-			u = dao.isEmailExist(user.getEmail());
-			if(u != null)
-				return new Feedback(false, "L'adresse email est déjà utilisée");
-			
-			
-			String hashedPassword = Utils.hashMD5(user.getPassword());
-			if(hashedPassword == null)
-				return new Feedback(false, "An error occurs during hashing");
-			user.setPassword(hashedPassword);
-			
-			dao.insert(user.getName(), user.getPassword(), user.getEmail());
+			fb = isValidUser(user);
+			if(fb.isSuccess()) {
+
+				// on hashe le mdp pour le protéger
+				String hashedPassword = Utils.hashMD5(user.getPassword());
+				if(hashedPassword == null)
+					return new Feedback(false, "An error occurs during hashing");
+				user.setPassword(hashedPassword);
+
+				// on insert l'utilisateur dans la bdd.
+				dao.insert(user.getName(), user.getPassword(), user.getEmail());
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			return new Feedback(false, "An error occurs during insertion to database");
 		}
+		return fb;
+	}
+
+	/**
+	 * Vérifie si l'utilisateur existe déjà (pseudo/email), si il est valide (email correcte, pseudo correct, mdp correct...)
+	 * @param u
+	 * @return
+	 */
+	private Feedback isValidUser(User user) {
+		// test le pseudo
+		if(!user.getName().matches("^[a-zA-Z0-9À-ÿ-]{3,20}$")) {
+			return new Feedback(false, "Le pseudo est invalide !");
+		}
+
+		// Test si le pseudo existe déjà ou non
+		User u = dao.isNameExist(user.getName());
+		if(u != null)
+			return new Feedback(false, "Le pseudo est déjà utilisé");
+
+
+
+		// test si l'email est correcte
+		if(!user.getEmail().matches("^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\\.[a-z]{2,4}$")) {
+			return new Feedback(false, "L'adresse mail est incorrecte");
+		}
+
+		// Test si l'email a déjà été utilisée
+		u = dao.isEmailExist(user.getEmail());
+		if(u != null)
+			return new Feedback(false, "L'adresse email est déjà utilisée");
+
+		
+		// test si le mdp est correct
+		if(user.getPassword().length() < 6) {
+			return new Feedback(false, "Le mot de passe doit faire au moins 6 caractères !");
+		}
+
 		return new Feedback(true, "Register OK");
 	}
-	
-	
+
+
 	@POST
 	@Path("/login")
 	public Feedback logUser(User user) {
@@ -63,7 +97,7 @@ public class UserResource {
 		if(hashedPassword == null)
 			return new Feedback(false, "An error occurs during hashing");
 		user.setPassword(hashedPassword);
-		
+
 		try {
 			User u = dao.userIsCorrect(user.getName(), user.getPassword());
 			if(u == null) 
@@ -74,7 +108,7 @@ public class UserResource {
 		}
 		return new Feedback(true, "Login OK");
 	}
-	
+
 
 	@GET
 	@Path("/{id}")
@@ -85,8 +119,8 @@ public class UserResource {
 		}
 		return out;
 	}
-	
-	
+
+
 	@GET
 	public List<User> getUsers() {
 		List<User> out = dao.getAll();
