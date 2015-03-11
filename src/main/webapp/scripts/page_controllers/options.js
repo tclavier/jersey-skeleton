5649afbe9e3360a06a667dae997cb03d8dff4224
Tanguy <tanguy.depommier@etudiant.univ-lille1.fr>
2015-03-11 15:27:43 +0100

    Page de profil + Page d'options (en cours) + tool type sur quelques pages

--- src/main/webapp/scripts/page_controllers/options.js
+++ src/main/webapp/scripts/page_controllers/options.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
 
 
+	function showProfileInfo(data) {
+		$("#info_player").append("<b> Pseudo :</b> " + data.user.name+"  ");
+		$("#info_player").append("<input class='btn btn-default btn-xs' type='button' value='Modifier'> <br> <br>");
+		$("#info_player").append("<b> Email :</b> " + data.user.email+"  ");
+		$("#info_player").append("<input class='btn btn-default btn-xs' type='button' value='Modifier'> <br> <br>");
+		$("#info_player").append("<b> Mot de passe :</b> ******  ");
+		$("#info_player").append("<input class='btn btn-default btn-xs' type='button' value='Modifier'> <br>");
+
+	}
+
+
 	function enableFacebook(enable) {
 		FB.getLoginStatus(function(response) {
 			if (response.status === 'connected') {
@@ -11,7 +22,7 @@ $(document).ready(function() {
 					console.log(response);
 					if(response.status === "connected")
 						$("#login_fb").html("Connecté à facebook !")
-					
+
 
 				}, {scope: 'public_profile,user_friends'});
 			}
@@ -22,8 +33,8 @@ $(document).ready(function() {
 	$("#login_fb").click(function() {
 		enableFacebook(true);
 	});
-	
-	
+
+
 	/**
 	 * INIT FB SDK
 	 */
@@ -42,5 +53,22 @@ $(document).ready(function() {
 		js.src = "http://connect.facebook.net/fr_FR/sdk.js";
 		fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
+
+
+	if(location.hash == "") {
+		$.getJSON("v1/profile/me/" + document.cookie, function(data) {
+			console.log(data);
+			showProfileInfo(data);
+		})
+		.error(function() {
+			// Utilisateur non loggé
+			location.replace("/");
+		});
+	} else {
+		$.getJSON("v1/profile/" + location.hash.substring(1), function(data) {
+			console.log(data);
+			showProfileInfo(data);
+		});
+	}
 
 });
