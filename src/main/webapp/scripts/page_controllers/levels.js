c10d328e111927bdb046f698e1a052552431ee29
Nicolas <nicolas.crosetti@gmail.com>
2015-03-26 10:35:28 +0100

    affichage auteur liste

--- src/main/webapp/scripts/page_controllers/levels.js
+++ src/main/webapp/scripts/page_controllers/levels.js
@@ -2,24 +2,35 @@ $(document).ready(function() {
 	/**/
 
 	
-	function loadLevelList() {
+	function loadLevelList(userNames) {
 		$.getJSON("v1/levelLists", function(data) {
-			showLevels(data);
+			showLevels(data, userNames);
 		});
 	}
 	
-	function showLevels(data) {
+	function loadUserNames() {
+		var userNames = [];
+		$.getJSON("v1/users", function(data) {			
+			for (var i = 0; i < data.length; i++) {
+				userNames[data[i].id] = data[i].name;
+			}
+			loadLevelList(userNames);
+		});
+	}
+	
+	function showLevels(data, userNames) {
 		for(var i = 0 ; i < data.length ; i++) {
 			var thumbnail = $('<div class="thumbnail level-element"></div>');
 			var caption = $('<div class="caption"></div>');
 			caption.append($('<h3>' + data[i].name + '</h3> <h5>' + data[i].levelCount + ' niveaux</h5>'));
+			caption.append($('<h6>Auteur: <a href="profile.html?id=' + data[i].idAuthor +'" >' 
+					+ userNames[data[i].idAuthor] + '</a></h6>'));
 			caption.append($('<p><a href="game.html?level=0&list=' + data[i].id + '" class="btn btn-primary level_preview" role="button">Jouer !</a></p>'));
 			thumbnail.append(caption);
-			console.log(thumbnail);
 			$("#tutorial_lists").append(thumbnail);
 		}
 	}
 	
 	
-	loadLevelList();
+	loadUserNames();
 });
