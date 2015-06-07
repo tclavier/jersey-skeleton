444e200dc4807a4d17ecb6b6c4ef5998b7d4355a
Thomas Clavier <tom@tcweb.org>
2015-06-07 15:08:30 +0200

    cleanup DB to have predictible tests

--- src/test/java/fr/iutinfo/RestClientTest.java
+++ src/test/java/fr/iutinfo/RestClientTest.java
@@ -7,22 +7,35 @@ import java.util.List;
 import javax.ws.rs.core.Application;
 
 import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Before;
 import org.junit.Test;
+import org.skife.jdbi.v2.DBI;
 
 public class RestClientTest extends JerseyTest {
-	@Override
-    protected Application configure() {
-        return new App();
-    }
+    private static UserDao dao;
 
-	@Test
-	public void getUsers () {
-		int port = this.getPort();
-		String baseUrl = "http://localhost:"+port+"/user/";
-		RestClient client = new RestClient();
-		client.addUser(new User(0, "Thomas"),  baseUrl );
-		client.addUser(new User(0, "Yann"),  baseUrl );
-		List<User> users = client.getUrlAsUser(baseUrl );
-		assertEquals(2, users.size());
-	}
+  @Override
+  protected Application configure() {
+      App app = new App();
+      DBI dbi = app.dbi;
+      dao = dbi.open(UserDao.class);
+    return new App();
+  }
+
+  @Before
+  public void cleanupDb() {
+      dao.dropUserTable();
+      dao.createUserTable();
+  }
+
+  @Test
+  public void getUsers () {
+    int port = this.getPort();
+    String baseUrl = "http://localhost:"+port+"/user/";
+    RestClient client = new RestClient();
+    client.addUser(new User(0, "Thomas"),  baseUrl );
+    client.addUser(new User(0, "Yann"),  baseUrl );
+    List<User> users = client.getUrlAsUser(baseUrl );
+    assertEquals(2, users.size());
+  }
 }
