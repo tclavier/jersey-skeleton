c81436738881b13e84f39e5fcd1cacca19e0e2d8
Thomas Clavier <tom@tcweb.org>
2016-10-24 10:03:25 +0200

    Format

--- serveur/src/main/java/fr/iutinfo/skeleton/api/UserDao.java
+++ serveur/src/main/java/fr/iutinfo/skeleton/api/UserDao.java
@@ -7,31 +7,31 @@ import org.skife.jdbi.v2.tweak.BeanMapperFactory;
 import java.util.List;
 
 public interface UserDao {
-	@SqlUpdate("create table users (id integer primary key autoincrement, name varchar(100), alias varchar(100), email varchar(100), passwdHash varchar(32), salt varchar(32))")
-	void createUserTable();
+    @SqlUpdate("create table users (id integer primary key autoincrement, name varchar(100), alias varchar(100), email varchar(100), passwdHash varchar(32), salt varchar(32))")
+    void createUserTable();
 
-	@SqlUpdate("insert into users (name,alias,email, passwdHash, salt) values (:name, :alias, :email, :passwdHash, :salt)")
-	@GetGeneratedKeys
-	int insert(@BindBean() User user);
+    @SqlUpdate("insert into users (name,alias,email, passwdHash, salt) values (:name, :alias, :email, :passwdHash, :salt)")
+    @GetGeneratedKeys
+    int insert(@BindBean() User user);
 
-	@SqlQuery("select * from users where name = :name")
+    @SqlQuery("select * from users where name = :name")
     @RegisterMapperFactory(BeanMapperFactory.class)
-	User findByName(@Bind("name") String name);
+    User findByName(@Bind("name") String name);
 
-	@SqlQuery("select * from users where name like :name")
-	@RegisterMapperFactory(BeanMapperFactory.class)
-	List<User> search(@Bind("name") String name);
+    @SqlQuery("select * from users where name like :name")
+    @RegisterMapperFactory(BeanMapperFactory.class)
+    List<User> search(@Bind("name") String name);
 
-	@SqlUpdate("drop table if exists users")
-	void dropUserTable(); 
+    @SqlUpdate("drop table if exists users")
+    void dropUserTable();
 
-	@SqlQuery("select * from users order by id")
-	@RegisterMapperFactory(BeanMapperFactory.class)
-	List<User> all();
+    @SqlQuery("select * from users order by id")
+    @RegisterMapperFactory(BeanMapperFactory.class)
+    List<User> all();
 
-	@SqlQuery("select * from users where id = :id")
-	@RegisterMapperFactory(BeanMapperFactory.class)
-	User findById(@Bind("id") int id);
+    @SqlQuery("select * from users where id = :id")
+    @RegisterMapperFactory(BeanMapperFactory.class)
+    User findById(@Bind("id") int id);
 
-	void close();
+    void close();
 }
--- serveur/src/main/java/fr/iutinfo/skeleton/api/UserResource.java
+++ serveur/src/main/java/fr/iutinfo/skeleton/api/UserResource.java
@@ -11,45 +11,44 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class UserResource {
-	private static UserDao dao = BDDFactory.getDbi().open(UserDao.class);
     final static Logger logger = LoggerFactory.getLogger(UserResource.class);
-
+    private static UserDao dao = BDDFactory.getDbi().open(UserDao.class);
 
     public UserResource() {
-		try {
-			dao.createUserTable();
-			dao.insert(new User(0,"Margaret Thatcher", "la Dame de fer"));
-		} catch (Exception e) {
-			logger.debug("Table déjà là !");
-		}
-	}
-	
-	@POST
-	public User createUser(User user) {
+        try {
+            dao.createUserTable();
+            dao.insert(new User(0, "Margaret Thatcher", "la Dame de fer"));
+        } catch (Exception e) {
+            logger.debug("Table déjà là !");
+        }
+    }
+
+    @POST
+    public User createUser(User user) {
         user.resetPasswordHash();
         int id = dao.insert(user);
         user.setId(id);
-		return user;
-	}
+        return user;
+    }
 
-	@GET
-	@Path("/{name}")
-	public User getUser(@PathParam("name") String name) {
-		User user = dao.findByName(name);
-		if (user == null) {
-			throw new WebApplicationException(404);
-		}
-		return user;
-	}
+    @GET
+    @Path("/{name}")
+    public User getUser(@PathParam("name") String name) {
+        User user = dao.findByName(name);
+        if (user == null) {
+            throw new WebApplicationException(404);
+        }
+        return user;
+    }
 
-	@GET
-	public List<User> getAllUsers(@QueryParam("q") String query) {
-		if (query == null) {
-			return dao.all();
-		} else {
-			logger.debug("Search users with query: " + query);
-			return dao.search("%"+query+"%");
-		}
-	}
+    @GET
+    public List<User> getAllUsers(@QueryParam("q") String query) {
+        if (query == null) {
+            return dao.all();
+        } else {
+            logger.debug("Search users with query: " + query);
+            return dao.search("%" + query + "%");
+        }
+    }
 
 }
